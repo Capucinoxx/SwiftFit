bb6cdbb2d7d1d873bdd2f1f5ac20794a618da44e
Jacty <saej3101@usherbrooke.ca>
2021-09-18 18:37:55 -0400

    Rearrange Model and Clothes fucntions

--- src/algo/Clothes.py
+++ src/algo/Clothes.py
@@ -8,21 +8,9 @@ class Clothes():
         self.filename = filename
         self.type = None
 
-    def load_image(self, filename):
-        # load the image
-        img = image.load_img(filename, grayscale=True, target_size=(28, 28))
-        # convert to array
-        img = image.img_to_array(img)
-        # reshape into a single sample with 1 channel
-        img = img.reshape(1, 28, 28, 1)
-        # prepare pixel data
-        img = img.astype('float32')
-        img = img / 255.0
-        return img
-
     def get_type(self):
         # load the image
-        img = self.load_image(self.filename)
+        img = self.model.load_image(self.filename)
 
         img_model = self.model.img_model
         # predict the class
--- src/algo/model.py
+++ src/algo/model.py
@@ -1,11 +1,24 @@
 import tensorflow as tf
-# from tests import load_image
 import numpy as np
+from tensorflow.keras.preprocessing import image
 
 class Model(object):
     class __Model():
         def __init__(self):
             self.img_model = tf.keras.models.load_model('final_model.h5')
+
+        def load_image(self, filename):
+            # load the image
+            img = image.load_img(filename, grayscale=True, target_size=(28, 28))
+            # convert to array
+            img = image.img_to_array(img)
+            # reshape into a single sample with 1 channel
+            img = img.reshape(1, 28, 28, 1)
+            # prepare pixel data
+            img = img.astype('float32')
+            img = img / 255.0
+            
+            return img
             
     instance = None
 
@@ -13,6 +26,7 @@ class Model(object):
         if not Model.instance:
             Model.instance = Model.__Model()
         return Model.instance 
+
 
 # x = Model()
 # model = x.img_model
