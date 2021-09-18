de7a206add084a250847d5bf13e21da9094880e1
capucinoxx <bendertermebc@gmail.com>
2021-09-18 05:14:08 -0400

    [feat] - correctif db + login/register user

--- src/models.py
+++ src/models.py
@@ -1,27 +1,32 @@
 from src import db
 
 class User(db.Model):
+  __tablename__ = 'user'
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  email = db.Column(db.String(128))
-  password = db.Column(db.String(128))
+  email = db.Column(db.String(128), unique_key=True)
+  password = db.Column(db.String(256))
 
 class Type:
+  __tablename__ = 'type'
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   desc = db.Column(db.String(50))
 
 class Clothes:
+  __tablename__ = 'clothes'
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  type = db.relationship(Type, backref='clothes')
-  user = db.relationship(User, backref='clothes')
+  image = db.Column(db.LargeBinary)
+  type = db.Column(db.Integer)
+  user = db.Column(db.Integer)
   color = db.Column(db.Integer)
 
 class History(db.Model):
+  __tablename__ = 'history'
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   created = db.Column(db.DateTime)
-  user = db.relationship(User, backref='history')
-  pants = db.relationship(Clothes, backref='clothes')
-  shirt = db.relationship(Clothes, backref='clothes')
-  shoe = db.relationship(Clothes, backref='clothes')
+  user = db.Column(db.Integer)
+  pants = db.Column(db.Integer)
+  shirt = db.Column(db.Integer)
+  shoe = db.Column(db.Integer)
 
 
 
