a2b85a900a58989577dd0b6d1b0d461770292edd
ciwrithon <mariesoleilrioux@gmail.com>
2021-09-18 19:08:48 -0400

    closet visual

--- app/components/closet.js
+++ app/components/closet.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FlatList, TouchableOpacity, View, Text, Image, StyleSheet, Dimensions } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
 import background from '../ressources/background_closet.png'
+import slide_arrow from '../ressources/slide_arrow.png'
 
 const API_KEY = '563492ad6f917000010000011ce76314e86a4f05abf1138ca17632ef'
 const THUMB_SIZE = 80
@@ -30,7 +32,7 @@ const Category = (props) => {
 
     <View style={{flex: 1, flexBasis: 115 }}>
       
-      <Text style={{ fontSize: 22 }}>{ props.title }</Text>
+      <Text style={{ fontSize: 22, fontFamily: "Futura", color: "#492d35" }}>{ props.title }</Text>
       <FlatList
         data={props.photos}
         keyExtractor={(item) => item.id.toString()}
@@ -51,6 +53,7 @@ const Category = (props) => {
                   marginRight: 10,
                   borderRadius: 12,
                   borderWidth: 2,
+                  borderColor: "#fff",
                 }}
               >
                 
@@ -75,8 +78,9 @@ export default () => {
   }, [])
 
   const backgroundUri = Image.resolveAssetSource(background).uri
+  const slideUri = Image.resolveAssetSource(slide_arrow).uri
   return (
-    <View style={{ width, height, flex: 1, backgroundColor: "#fff" }}>
+    <View style={{ width, height, flex: 1, backgroundColor: "#fff", position:"relative"}}>
       <Image 
         source={{
           uri: backgroundUri
@@ -84,13 +88,22 @@ export default () => {
         resizeMode="cover"
         style={[StyleSheet.absoluteFillObject, { opacity: 1 }]}
       />
-      <View style={{ flex: 1, paddingLeft: 10, paddingRight: 10, paddingTop: 40}}>
-      <Text>{"Choose an"}</Text>
-      <View style={{  flex:1, flexDirection: 'column' }}>
-        <Category title={"Pantalons"} photos={photos}/>
-        <Category title={"Shirt"} photos={photos}/>
-        <Category title={"Chandaille manche courte"} photos={photos}/>
-        <Category title={"Souliers"} photos={photos}/>
+      <Image
+        source={{
+          uri: slideUri
+        }}
+        style={{position: "absolute", top:55, left:20, zIndex: 10, width: 70, height: 70, opacity: 1}}
+      />
+      <View style={{ flex: 1, paddingLeft: 10, paddingRight: 10, paddingTop: 40 }}>
+      <Text style={{ color:"#fff", fontSize: 30, textAlign: 'center', marginTop: 31.5, marginBottom: 20,
+      fontWeight: "bold", fontFamily: "Futura" }}>{"Choose an item"}</Text>
+      <Text style={{ position: "absolute", marginTop: 70, marginLeft: 80, color:"#492d35", fontSize: 30, textAlign: 'center', fontWeight: "bold", fontFamily: "Futura" }}>{"Choose an item"}</Text>
+      <View style={{ flex:1, flexDirection: 'column' }}>
+        <Category title={"Shirts"} photos={photos}/>
+        <Category title={"Outerwear"} photos={photos}/>
+        <Category title={"Bottoms"} photos={photos}/>
+        <Category title={"Dresses/skirts"} photos={photos}/>
+        <Category title={"Shoes"} photos={photos}/>
     </View>
     </View>
     </View>
