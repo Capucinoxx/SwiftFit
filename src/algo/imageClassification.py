710a70dfc60c36936dda858bc5c59d051eaa645b
Jacty <saej3101@usherbrooke.ca>
2021-09-17 23:29:10 -0400

    Data normalization

--- src/algo/imageClassification.py
+++ src/algo/imageClassification.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.datasets import fashion_mnist 
+from tensorflow.keras.utils import to_categorical
+
 
 # Download Zalando Fashion MNIST data
 (training_x, training_y), (tests_x, tests_y) = fashion_mnist.load_data()
@@ -7,3 +9,11 @@ from tensorflow.keras.datasets import fashion_mnist
 # Reshape data (1, 28, 28, 1)
 training_x = training_x.reshape(training_x.shape[0], 28, 28, 1)
 tests_x = tests_x.reshape(tests_x.shape[0], 28, 28, 1)
+
+# One-hot encoding
+training_y = to_categorical(training_y)
+tests_y = to_categorical(tests_y)
+
+# Float conversion and data normalization 
+training_x = training_x.astype('float32') / 255
+tests_x = tests_x.astype('float32') / 255
